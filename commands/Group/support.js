4e612983aed2515bb12127efced5163856aff4ff
abd-khadher <116655509+abd-khadher@users.noreply.github.com>
2022-10-29 14:04:22 +0530

    Success ✔️

--- commands/Group/support.js
+++ commands/Group/support.js
@@ -3,12 +3,12 @@ const { tlang } = require('../../lib')
 module.exports = {
     name: 'support',
     category: 'weeb',
-    desc: 'Sends SECKTOR userbot group link.',
+    desc: 'Sends WICKIE userbot group link.',
     async exec(citel, Void,args) {
         citel.reply(`*Check your Pm ${tlang().greet}*`);
         await Void.sendMessage(`${citel.sender}`, {
           image: log0,
-          caption: `*Group Name: Secktor-Support*\n*Group Link:* https://chat.whatsapp.com/Bl2F9UTVU4CBfZU6eVnrbC`,
+          caption: `*Group Name: wickie-Support*\n*Group Link:* https://chat.whatsapp.com/HMJQZqjClNgKOitnskMpbJ`,
         });
  
     }
